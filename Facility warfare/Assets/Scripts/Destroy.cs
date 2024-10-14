a6b1b15a741df7b093afc6b2cdeea8e0e3850039
UlzitonGit <anton.ulzibat@gmail.com>
2024-10-14 18:12:07 +0300

    ragdoll

--- Facility warfare/Assets/Scripts/Destroy.cs
+++ Facility warfare/Assets/Scripts/Destroy.cs
@@ -12,7 +12,7 @@ public class Destroy : MonoBehaviour
 
     IEnumerator Destroying()
     {
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(3);
         Destroy(gameObject);
     }
 }
